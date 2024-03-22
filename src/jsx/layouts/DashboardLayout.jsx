8ef941e2413ed9dea3eb1dff02101ccc262164cc
alexesba <alexesba@gmail.com>
2024-03-22 14:44:35 -0600

    improve the DashboarLayout, load the current user data if is authenticated

--- src/jsx/layouts/DashboardLayout.jsx
+++ src/jsx/layouts/DashboardLayout.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import Footer from './Footer';
@@ -14,31 +14,36 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 import useSessionActions from '../../store/actions/sessionActions';
 import LockScreen from '../pages/LockScreen';
+import currentUserAtom from '../../store/atoms/currentUserAtom';
+import useAuthActions from '../../store/actions/authActions';
 
 function DashboardLayout() {
   const { sidebariconHover } = useContext(ThemeContext);
   const sideMenu = useRecoilValue(toggleMenuAtom);
+  const currentUser = useRecoilValue(currentUserAtom);
   const windowsize = window.innerWidth;
+  const { loadCurrentUserBytoken } = useAuthActions();
   const { isLoggedIn } = useSessionActions();
 
-  // console.log('isLoggedIn', isLoggedIn());
-  // return <LockScreen />;
+  if (!isLoggedIn()) return <Navigate to={LOGIN_PATH} />;
 
-  return (isLoggedIn()
-    ? (
-      <div id="main-wrapper" className={` show  ${sidebariconHover ? 'iconhover-toggle' : ''} ${sideMenu ? 'menu-toggle' : ''}`}>
-        <div className={`wallet-open  ${windowsize > 1920 ? 'active' : ''}`}>
-          <Nav2 />
-          <div className="content-body" style={{ minHeight: window.screen.height + 20 }}>
-            <div className="container-fluid">
-              <Outlet />
-            </div>
+  loadCurrentUserBytoken();
+
+  if (!currentUser) return null;
+
+  return (
+    <div id="main-wrapper" className={` show  ${sidebariconHover ? 'iconhover-toggle' : ''} ${sideMenu ? 'menu-toggle' : ''}`}>
+      <div className={`wallet-open  ${windowsize > 1920 ? 'active' : ''}`}>
+        <Nav2 />
+        <div className="content-body" style={{ minHeight: window.screen.height + 20 }}>
+          <div className="container-fluid">
+            <Outlet />
           </div>
-          <Footer changeFooter="footer-outer" />
-          <WalletBar />
         </div>
+        <Footer changeFooter="footer-outer" />
+        <WalletBar />
       </div>
-    ) : <Navigate to={LOGIN_PATH} />
+    </div>
   );
 }
 
