84f9fc170b69ff99154b472131b88798cbf4694e
alexesba <alexesba@gmail.com>
2024-03-22 16:28:48 -0600

    Add gender validation for students

--- src/jsx/components/Student/validations/createStudent.js
+++ src/jsx/components/Student/validations/createStudent.js
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-const EMAIL_REGX = /^(?!.*@[^,]*,)/;
+const EMAIL_REGX = /^[^@\s]+@[^@.\s]+(?:\.[^@.\s]+)+$/;
 
 const createStudentSchema = yup.object().shape({
   first_name: yup.string().required('Fist name is required'),
@@ -8,6 +8,8 @@ const createStudentSchema = yup.object().shape({
   middle_name: yup.string().required('Last name is required'),
   email: yup.string().matches(EMAIL_REGX, 'Email is invalid').required('Email is required'),
   date_of_birth: yup.date().required('Date of Birth is required'),
+  gender: yup.string().required('Gender is required'),
+  organization_id: yup.string().required('Organization is required'),
   mobile_phone: yup.string().required('Phone number is required')
     .min(10, 'Phone number must have 10 digits')
     .max(10, 'Phone number must have 10 digits'),
@@ -22,9 +24,11 @@ const createStudentSchema = yup.object().shape({
     last_name: yup.string().required('Last name is required'),
     middle_name: yup.string().required('Last name is required'),
     email: yup.string().matches(EMAIL_REGX).required('Email is required'),
+    organization_id: yup.string().required('Organization is required'),
     mobile_phone: yup.string().required('Phone number is required')
       .min(10, 'Phone number must have 10 digits')
       .max(10, 'Phone number must have 10 digits'),
+    gender: yup.string().required('Gender is required'),
   })),
 
 });
