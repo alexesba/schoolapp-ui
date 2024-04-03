ba901b21eb2fbf78f8e2df6b5b1c9450b970361d
alexesba <alexesba@gmail.com>
2024-04-03 10:05:44 -0600

    Add link to the program show view for Teacher subjects

--- src/jsx/components/Teacher/Subjects.jsx
+++ src/jsx/components/Teacher/Subjects.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import useProgramActions from '../../../store/actions/programActions';
 
 function Subjects({ subjects: { data } }) {
